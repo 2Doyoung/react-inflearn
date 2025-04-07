86b2f205c695d1f29111fdd0a815a9244edef731
2Doyoung <dlehdud1653@kakao.com>
2025-04-07 12:39:43 +0900

    composition vs interitance

---
+++ my-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card title="Hi" backgroundColor="#4ea04e">
+            <p>안녕하세요, 리액트</p>
+            <p>리액트 사용을 하고 있습니다.</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
